8a3336f904b844e0a9e28bbcea822ac74de812ac
jaidang2900-droid <jaidang2900@gmail.com>
2025-11-12 00:21:37 +0530

    Update 103.c

---
+++ Day53/103.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int totalSum = 0;
+    for (int i = 0; i < n; i++)
+        totalSum += arr[i];
+
+    int leftSum = 0, pivot = -1;
+
+    for (int i = 0; i < n; i++) {
+        int rightSum = totalSum - leftSum - arr[i]; // Sum on the right
+
+        if (leftSum == rightSum) {
+            pivot = i;
+            break; // Leftmost pivot index found
+        }
+
+        leftSum += arr[i]; // update left sum
+    }
+
+    printf("%d", pivot);
+
+    return 0;
+}
